0656e2e453436e87e15f15ea0345456a46202297
acalleia <alex_calleia@yahoo.com>
2019-02-14 13:48:10 -0500

    Add files via upload
    adjusting for responsiveness

--- src/main.js
+++ src/main.js
@@ -34,37 +34,92 @@ $(function () {
         };
     });
 
+    $('.navbar-nav>li>a').on('click', function(){
+        $('.navbar-collapse').collapse('hide');
+    });
+
     $('#gene').on('click', function(){
         if($('#general').css('display', 'none')){
+            $('#madameP').mouseenter(function(){
+                $(this).css("color", "#009395")
+            });
+            $('#madameP').mouseleave(function(){
+                $(this).css("color", "white")
+            });
+            $('#gene').mouseenter(function(){
+                $(this).css("color", "white")
+            });
+            $('#gene').mouseleave(function(){
+                $(this).css("color", "#eab25c")
+            });
             $('#general').css("display", "grid");
             $('#gene').css({"color": "#eab25c", "background-color": "#009395"}) 
             $('#madameP').css({"color": "white", "background-color": "#eab25c"});
             $('#paulette').css('display', 'none');
+            
         }
     })
 
     $('#madameP').on('click', function(){
         if($('#paulette').css('display', 'none')){
+            $('#gene').mouseenter(function(){
+                $(this).css("color", "#009395")
+            });
+            $('#gene').mouseleave(function(){
+                $(this).css("color", "white")
+            });
+            $('#madameP').mouseenter(function(){
+                $(this).css("color", "white")
+            });
+            $('#madameP').mouseleave(function(){
+                $(this).css("color", "#eab25c")
+            });
             $('#paulette').css("display", "grid"); 
             $('#madameP').css({"color": "#eab25c", "background-color": "#009395"});
             $('#gene').css({"color": "white", "background-color": "#eab25c"}) 
             $('#general').css('display', 'none');
+            
         }
     })
 
     $('#gen').on('click', function(){
         if($('#general').css('display', 'none')){
+            $('#madameP').mouseenter(function(){
+                $(this).css("color", "#009395")
+            });
+            $('#madameP').mouseleave(function(){
+                $(this).css("color", "white")
+            });
+            $('#gene').mouseenter(function(){
+                $(this).css("color", "white")
+            });
+            $('#gene').mouseleave(function(){
+                $(this).css("color", "#eab25c")
+            });
             $('#general').css("display", "grid");
             $('#gene').css({"color": "#eab25c", "background-color": "#009395"}) 
             $('#madameP').css({"color": "white", "background-color": "#eab25c"});
             $('#paulette').css('display', 'none');
+            
         }
     })
     $('#pau').on('click', function(){
         if($('#paulette').css('display', 'none')){
+            $('#gene').mouseenter(function(){
+                $(this).css("color", "#009395")
+            });
+            $('#gene').mouseleave(function(){
+                $(this).css("color", "white")
+            });
+            $('#madameP').mouseenter(function(){
+                $(this).css("color", "white")
+            });
+            $('#madameP').mouseleave(function(){
+                $(this).css("color", "#eab25c")
+            });
             $('#paulette').css("display", "grid"); 
             $('#madameP').css({"color": "#eab25c", "background-color": "#009395"});
-            $('#gene').css({"color": "white", "background-color": "#eab25c"}) 
+            $('#gene').css({"color": "white", "background-color": "#eab25c"})
             $('#general').css('display', 'none');
         }
     })
